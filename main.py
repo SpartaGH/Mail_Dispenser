8f83f29fdfe9fda25c716f5473664b2aa9137dcf
JulienPH <julien.poirot-hattermann5@etu.univ-lorraine.fr>
2018-07-31 00:09:43 +0200

    tentative connexion
    cela ne marche pas

--- main.py
+++ main.py
@@ -3,13 +3,7 @@ import re
 
 def verifnomb():
     quant = input("Nombre de mails à envoyer ?")
-    return quant
-
-# while True:
-#       if type(verifnomb()) != int:
-#           verifnomb()
-#       else
-#           break
+    return int(quant)
 
 def main():
 
@@ -17,35 +11,38 @@ def main():
     fichier = 'liste.txt'
 
     #choisir le nb de mail à envoyé
-    for(nbmail=null; type(nbmail) != int; nbmail=verifnomb())
+    nbmail=verifnomb()
+    while type(nbmail) != int:
+        nbmail=verifnomb()
 
     with open(fichier, 'r') as f:
         
         #Login
+        print("test1")
+        #☻ça bug au niveau de la connexion
         server = smtplib.SMTP('smtp-gmail.com', 465)    #je connais pas cette fonction, je vais te faire confiance
         server.starttls()
         email = 'louisph54300@gmail.com'
         mdp = 'v#OKIF9QuxSpYM5Z'
         server.login(email, mdp)
         print(server.noop()) #Retourne code connexion (refusé/accépté)
-        
+        print("test2")
 
         #boucle sur chaque ligne -> 1 mail
         n=1
         for line in f:
-             
-            msg = "msg test n°"n
+            
+            msg = "msg test n°"+n
             client = str(line)  #controle avec des regex si chaque ligne est bien un mail valide
-        regexMail = "^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$"
+            regexMail = "^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$"
             if re.match(regexMail,client):
-            server.sendmail("sp email", client, msg)
-            print("Email n°"n" envoyé à "client", msg : "msg)
-            n++
-        else:
-            print("Mail illisible : "client)
+                server.sendmail("sp email", client, msg)
+                print("Email n°"+n+" envoyé à "+client+", msg : "+msg)
+                n=n+1
+            else:
+                print("Mail illisible : "+client)
 
         #Quit
         server.quit()
-
 
 main()
