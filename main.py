d95b4aea3edbaf10f3bca7157365cc35fb8d061d
LouisGH <louisph54300@gmail.com>
2018-07-30 23:26:05 +0200

    Set parametres

--- main.py
+++ main.py
@@ -22,10 +22,10 @@ def main():
 	with open(fichier, 'r') as f:
 	    
 		#Login
-		server = smtplib.SMTP('smtp-mail.outlook.com', 587)	#je connais pas cette fonction, je vais te faire confiance
+		server = smtplib.SMTP('smtp-gmail.com', 465)	#je connais pas cette fonction, je vais te faire confiance
 		server.starttls()
-		email = ''
-		mdp = ''
+		email = 'louisph54300@gmail.com'
+		mdp = 'v#OKIF9QuxSpYM5Z'
 		server.login(email, mdp)
 		print(server.noop()) #Retourne code connexion (refusé/accépté)
 		
@@ -38,7 +38,7 @@ def main():
 			client = str(line)	#faut controler avec des regex si chaque ligne est bien un mail valide
         regexMail = "^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$"
 			if re.match(regexMail,client):
-            server.sendmail("sp email", client, msg)	#c'est quoi sp email ?
+            server.sendmail("sp email", client, msg)	#c'est quoi sp email ? Stephane Plaze
             print("Email n°"n" envoyé à "client)
             n++
         else:
